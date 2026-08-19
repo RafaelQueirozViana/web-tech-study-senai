af6803183eb50bb9e754b3174f6d1f17a6ca9e85
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-08-19 11:29:18 -0300

    aprimorado css do site

--- web-tech-study-senai/introducao-react/src/App.jsx
+++ web-tech-study-senai/introducao-react/src/App.jsx
@@ -19,10 +19,25 @@ function App() {
       <div style={{ display: 'flex', gap: '20px', padding: '0px', margin: '80px 0' }}>
         <MovieCard title="Homem aranha sem volta para casa" rating="8.7">
           <p>Um grupo de amigos descobre segredos sobrenaturais e uma garota com poderes psicocinéticos na cidade de Hawkins.</p>
-          <strong>Gênero:</strong> Ficção Científica / Terror
+          <strong>Gênero:</strong> Ficção Científica / Aventura
         </MovieCard>
 
         <MovieCard title="Batman" rating="8.6">
+          <p>Órfão após o assassinato dos pais, um bilionário treina o corpo e a mente para combater o crime em sua cidade sob a identidade de um morcego vigilante.</p>
+          <strong>Gênero:</strong> Ficção Científica / Drama
+        </MovieCard>
+
+        <MovieCard title="Homem de ferro" rating="8.6">
+          <p>Bilionário genial cria armadura de alta tecnologia para salvar a própria vida e combater o mal.</p>
+          <strong>Gênero:</strong> Ficção Científica / Drama
+        </MovieCard>
+
+        <MovieCard title="Capitão América" rating="8.6">
+          <p>Um jovem franzino participa de um experimento secreto militar na Segunda Guerra Mundial para combater uma organização nazista rebelde..</p>
+          <strong>Gênero:</strong> Ficção Científica / Drama
+        </MovieCard>
+
+        <MovieCard title="Vingadores - Guerra Infinita" rating="8.6">
           <p>Uma equipe de exploradores viaja através de um buraco de minhoca no espaço na tentativa de garantir a sobrevivência da humanidade.</p>
           <strong>Gênero:</strong> Ficção Científica / Drama
         </MovieCard>
--- web-tech-study-senai/introducao-react/src/Components/card.jsx
+++ web-tech-study-senai/introducao-react/src/Components/card.jsx
@@ -1,16 +1,16 @@
-import imgCard from "../assets/img-card.jpg" 
+import imgCard from "../assets/homem-aranha-poster.jpg"
 import "../Components/card.css"
 
-const Card = () => { 
-  return ( 
-    <section> 
-      <article className="card"> 
-        <img src={imgCard} className="img-card"/> 
-        <p className="card-text ">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> 
-      </article> 
-      <hr /> 
-    </section> 
-  ) 
-} 
+const Card = () => {
+  return (
+    <section>
+      <article className="card">
+        <img src={imgCard} className="img-card" />
+        <p className="card-text ">Filmes em <A></A>lta no Momento: Homem Aranha sem volta para casa</p>
+      </article>
+      <hr />
+    </section>
+  )
+}
 
 export default Card
